8056786687754b614ba250362888c57bfea2eb7a
Kevin Lim <lyskevin@Kevins-MacBook-Pro.local>
2018-11-15 10:35:11 +0800

    Started working on ArrayDeque.java

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,36 @@
+/** Implements a deque using arrays */
+
+public class ArrayDeque<T> {
+    
+    private T[] array;
+    
+    public ArrayDeque() {
+        array = new T[8];
+    }
+    
+    public void addFirst(T item) {
+    }
+    
+    public void addLast(T item) {
+    }
+    
+    public boolean isEmpty() {
+    }
+    
+    public int size() {
+    }
+    
+    public void printDeque() {
+    }
+    
+    public T removeFirst() {
+    }
+    
+    public T removeLast() {
+    }
+    
+    public T get(int index) {
+        return array[index];
+    }
+    
+} // End ArrayDeque class
