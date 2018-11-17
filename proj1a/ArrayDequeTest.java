3da7b94bd96448510be1ee6ae8b658fb04a7959d
Kevin Lim <lyskevin@Kevins-MacBook-Pro.local>
2018-11-17 13:00:54 +0800

    Final update for Project 1A

--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -5,37 +5,6 @@ public class ArrayDequeTest {
         
         ArrayDeque<Integer> deque = new ArrayDeque<Integer>();
         
-        /*
-         for (int i = 0; i < 3; i++) {
-         deque.addFirst(i);
-         }
-         
-         deque.printDeque();
-         
-         for (int i = 0; i < 3; i++) {
-         deque.addLast(i);
-         }
-         
-         deque.printDeque();
-         
-         System.out.println(deque.get(4));
-         
-         for (int i = 0; i < 2; i++) {
-         deque.removeLast();
-         }
-         for (int i = 0; i < 2; i++) {
-         deque.removeFirst();
-         }
-         
-         System.out.println(deque.size());
-         deque.printDeque();
-         
-         for (int i = 0; i < 2; i++) {
-         deque.removeFirst();
-         }
-         
-         System.out.println(deque.isEmpty());*/
-        
         System.out.println(deque.size());
         System.out.println(deque.size());
         deque.addFirst(2);
