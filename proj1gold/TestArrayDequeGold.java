ce7b1f9da525beea75ce0285b7f68d30e4600dae
Kevin Lim <lyskevin@Kevins-MacBook-Pro.local>
2018-12-09 19:21:47 +0800

    Completed Project 1 Gold

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,45 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+    @Test
+    public void testDeque() {
+
+        ArrayDequeSolution<Integer> deque = new ArrayDequeSolution();
+        StudentArrayDeque<Integer> studentDeque = new StudentArrayDeque();
+        String errorMessage = "\n";
+
+        for (int i = 0; i < 10; i++) {
+            int randomNumber = StdRandom.uniform(100);
+            if (i % 2 == 0) {
+                deque.addFirst(randomNumber);
+                studentDeque.addFirst(randomNumber);
+                errorMessage += "addFirst(" + randomNumber + ")\n";
+            } else {
+                deque.addLast(randomNumber);
+                studentDeque.addLast(randomNumber);
+                errorMessage += "addLast(" + randomNumber + ")\n";
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Integer expected;
+            Integer actual;
+            if (i % 2 == 0) {
+                expected = deque.removeFirst();
+                actual = studentDeque.removeFirst();
+                errorMessage += "removeFirst()\n";
+                /*"Test failed!\nremoveFirst() should return " +
+                        expected + " but returned " + actual +
+                        " instead.\n";*/
+                assertEquals(errorMessage, expected, actual);
+            } else {
+                expected = deque.removeLast();
+                actual = studentDeque.removeLast();
+                errorMessage += "removeLast()\n";
+                assertEquals(errorMessage, expected, actual);
+            }
+        }
+
+    }
+}
