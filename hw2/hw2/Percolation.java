dc653e8830907dfa1d676a5ee6b501fd262ee895
Kevin Lim <lyskevin@Kevins-MacBook-Pro.local>
2019-01-13 12:51:56 +0800

    Refactored code in Percolation.java

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,7 +3,8 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 /**
- * Models a percolation system.
+ * Models a percolation system, which can be used in a Monte Carlo simulation
+ * to estimate the percolation threshold.
  *
  * Each site contains the following bitwise information about itself:
  * 0 => Closed site, 1 => Open site
@@ -15,112 +16,114 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
  * grid's top row, but not connected to the grid's bottom row. A site is
  * considered to be full if it is both open and connected to the grid's top
  * row. Each system of connected sites will have its own root site, which is
- * the component site returned by WeightedQuickUnionUF's find operation.
+ * the component site returned by WeightedQuickUnionUF's find method.
  * The system percolates if there is at least one root site which is
- * connected to the grid's top and bottom rows.
+ * connected to the grid's top and bottom rows. Decimal numbers are used
+ * throughout for bitwise operations for better readability.
+ *
+ * @author lyskevin
  */
+
 public class Percolation {
 
-    // Grid's attributes
+    /** System's attributes */
     private WeightedQuickUnionUF grid;
     private int[][] siteStatus;
-    private int N;
+    private int size; /** Grid's width and height */
     private int numOpenSites;
     private boolean hasPercolated;
 
-    // Create an N-by-N grid, with all sites initially blocked
-    public Percolation(int N) {
-        if (N <= 0) {
+    /**
+     * Create an n-by-n grid, with all sites initially blocked
+     * @param n The size of the grid
+     */
+    public Percolation(int n) {
+
+        if (n <= 0) {
             throw new IllegalArgumentException("N should be > 0");
         }
-        grid = new WeightedQuickUnionUF(N * N);
-        siteStatus = new int[N][N];
-        for (int row = 0; row < N; row++) {
-            for (int col = 0; col < N; col++) {
+
+        grid = new WeightedQuickUnionUF(n * n);
+        siteStatus = new int[n][n];
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
                 siteStatus[row][col] = 0;
-                if (row == 0) { // Top row site
+                if (row == 0) { /** Top row site */
                     siteStatus[row][col] |= 2;
                 }
-                if (row == N - 1) { // Bottom row site
+                if (row == n - 1) { /** Bottom row site */
                     siteStatus[row][col] |= 4;
                 }
             }
         }
-        this.N = N;
+        size = n;
         numOpenSites = 0;
         hasPercolated = false;
-    } // End Percolation constructor
 
-    // Open the site (row, col) if it is not open
+    } /** End Percolation constructor */
+
+    /**
+     * Open the site (row, col) if it is not open
+     * @param row The site's row number
+     * @param col The site's column number
+     */
     public void open(int row, int col) {
-        
-        if ((row < 0 || row >= N) || (col < 0 || col >= N)) {
+
+        if (!isValid(row) || !isValid(col)) {
             throw new IndexOutOfBoundsException("Invalid row or col index");
         }
 
-        // Check that site is closed
+        /** Only open site if it is closed */
         if (!isOpen(row, col)) {
 
-            // Open site
+            /** Open site */
             siteStatus[row][col] |= 1;
             numOpenSites++;
-            int site = rowAndColToInt(row, col);
+            int site = rowAndColToSite(row, col);
             boolean leftConnected = false;
             boolean rightConnected = false;
             boolean topConnected = false;
             boolean bottomConnected = false;
 
-            // Connect left site if necessary
+            /** Connect left site if necessary */
             int leftCol = col - 1;
-            if (leftCol >= 0 && isOpen(row, leftCol)) {
+            if (isValid(leftCol) && isOpen(row, leftCol)) {
+                connectSites(row, col, row, col - 1);
+                updateConnections(row, leftCol, row, col);
                 leftConnected = true;
-                int leftSite = rowAndColToInt(row, leftCol);
-                if (!grid.connected(site, leftSite)) {
-                    grid.union(site, leftSite);
-                    updateConnections(row, leftCol, row, col);
-                }
             }
 
-            // Connect right site if necessary
+            /** Connect right site if necessary */
             int rightCol = col + 1;
-            if (rightCol < N && isOpen(row, rightCol)) {
+            if (isValid(rightCol) && isOpen(row, rightCol)) {
+                connectSites(row, col, row, rightCol);
+                updateConnections(row, rightCol, row, col);
                 rightConnected = true;
-                int rightSite = rowAndColToInt(row, rightCol);
-                if (!grid.connected(site, rightSite)) {
-                    grid.union(site, rightSite);
-                    updateConnections(row, rightCol, row, col);
-                }
             }
 
-            // Connect top site if necessary
+            /** Connect top site if necessary */
             int topRow = row - 1;
-            if (topRow >= 0 && isOpen(topRow, col)) {
+            if (isValid(topRow) && isOpen(topRow, col)) {
+                connectSites(row, col, topRow, col);
+                updateConnections(topRow, col, row, col);
                 topConnected = true;
-                int topSite = rowAndColToInt(topRow, col);
-                if (!grid.connected(site, topSite)) {
-                    grid.union(site, topSite);
-                    updateConnections(topRow, col, row, col);
-                }
             }
 
-            // Connect bottom site if necessary
+            /** Connect bottom site if necessary */
             int bottomRow = row + 1;
-            if (bottomRow < N && isOpen(bottomRow, col)) {
+            if (isValid(bottomRow) && isOpen(bottomRow, col)) {
+                connectSites(row, col, bottomRow, col);
+                updateConnections(bottomRow, col, row, col);
                 bottomConnected = true;
-                int bottomSite = rowAndColToInt(bottomRow, col);
-                if (!grid.connected(site, bottomSite)) {
-                    grid.union(site, bottomSite);
-                    updateConnections(bottomRow, col, row, col);
-                }
             }
 
-            // Update root site
+            /** Update root site */
             int rootSite = grid.find(site);
-            int rootRow = intToRow(rootSite);
-            int rootCol = intToCol(rootSite);
+            int rootRow = siteToRow(rootSite);
+            int rootCol = siteToCol(rootSite);
             updateConnections(row, col, rootRow, rootCol);
 
-            // Update surrounding sites according to root
+            /** Update adjacent connected sites according to root site */
             if (leftConnected) {
                 updateConnections(rootRow, rootCol, row, leftCol);
             }
@@ -134,115 +137,154 @@ public class Percolation {
                 updateConnections(rootRow, rootCol, bottomRow, col);
             }
 
-            // Check if the system has percolated
+            /** Check if the system percolates */
             if (siteStatus[rootRow][rootCol] == 7) {
                 hasPercolated = true;
             }
 
         }
 
-    } // End open
+    } /** End open */
 
-    // Is the site (row, col) open?
+    /**
+     * Is the site (row, col) open?
+     * @param row The site's row number
+     * @param col The site's column number
+     */
     public boolean isOpen(int row, int col) {
-        if ((row < 0 || row >= N) || (col < 0 || col >= N)) {
-            throw new IndexOutOfBoundsException("Invalid row or col index");
-        }
-        return siteStatus[row][col] % 2 == 1;
-    } // End isOpen
 
-    // Is the site (row, col) full?
-    public boolean isFull(int row, int col) {
-        if ((row < 0 || row >= N) || (col < 0 || col >= N)) {
+        if (!isValid(row) || !isValid(col)) {
             throw new IndexOutOfBoundsException("Invalid row or col index");
         }
-        int site = rowAndColToInt(row, col);
+
+        return siteStatus[row][col] % 2 == 1;
+
+    } /** End isOpen */
+
+    /**
+     * Is the site (row, col) full?
+     * @param row The site's row number
+     * @param col The site's column number
+     */
+    public boolean isFull(int row, int col) {
+
+        if (!isValid(row) || !isValid(col)) {
+            throw new IndexOutOfBoundsException("Invalid row or col index");
+        }
+
+        int site = rowAndColToSite(row, col);
         int rootSite = grid.find(site);
-        int rootRow = intToRow(rootSite);
-        int rootCol = intToCol(rootSite);
+        int rootRow = siteToRow(rootSite);
+        int rootCol = siteToCol(rootSite);
         updateConnections(rootRow, rootCol, row, col);
         return siteStatus[row][col] % 4 == 3;
-    } // End isFull
 
-    // Number of open sites
+    } /** End isFull */
+
+    /** Number of open sites */
     public int numberOfOpenSites() {
         return numOpenSites;
-    } // End numOfOpenSites
+    } /** End numberOfOpenSites */
 
-    // Does the system percolate?
+    /** Does the system percolate? */
     public boolean percolates() {
         return hasPercolated;
-    } // End percolates
+    } /** End percolates */
 
-    // For unit testing
-    private void printGrid() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (isFull(i, j)) {
-                    System.out.print('F' + " ");
-                } else if (isOpen(i, j)) {
-                    System.out.print('O' + " ");
-                } else {
-                    System.out.print('C' + " ");
-                }
+    /** Runs some unit tests */
+    public static void main(String[] args) {
+
+    } /** End main */
+
+    /**************************** Helper Methods ****************************/
+
+    /**
+     * Returns true if the given row or column number is valid, else false
+     * @param rowOrCol The given row or column number
+     */
+    private boolean isValid(int rowOrCol) {
+        return rowOrCol >= 0 && rowOrCol < size;
+    } /** End isValid */
+
+    /**
+     * Converts the given row and column number into the site number
+     * @param row The site's row number
+     * @param col The site's column number
+     */
+    private int rowAndColToSite(int row, int col) {
+        return row * size + col;
+    } /** End rowAndColToSite */
+
+    /**
+     * Returns the given site's row number
+     * @param site The given site
+     */
+    private int siteToRow(int site) {
+        return site / size;
+    } /** End siteToRow */
+
+    /**
+     * Returns the given site's column number
+     * @param site The given site
+     */
+    private int siteToCol(int site) {
+        return site % size;
+    } /** End siteToCol */
+
+    /**
+     * Connects the two given sites
+     * Both sites must be open
+     * @param site1Row Site 1's row number
+     * @param site1Col Site 1's column number
+     * @param site2Row Site 2's row number
+     * @param site2Col Site 2's column number
+     */
+    private void connectSites(int site1Row, int site1Col,
+                              int site2Row, int site2Col) {
+        if (isValid(site1Row) && isValid(site1Col)
+                && isValid(site2Row) && isValid(site2Col)) {
+            int site1 = rowAndColToSite(site1Row, site1Col);
+            int site2 = rowAndColToSite(site2Row, site2Col);
+            if (!grid.connected(site1, site2)) {
+                grid.union(site1, site2);
             }
-            System.out.println();
         }
-        if (percolates()) {
-            System.out.println("The system percolates");
-        } else {
-            System.out.println("The system does not percolate");
-        }
-    } // End printGrid
+    } /** End connectSites */
 
-    // Converts 2D coordinates into a single number
-    private int rowAndColToInt(int row, int col) {
-        return row * N + col;
-    } // End rowAndColToInt
-
-    // Returns the row in the grid that the given number belongs to
-    private int intToRow(int num) {
-        return num / N;
-    } // End intToRow
-
-    // Returns the column in the grid that the given number belongs to
-    private int intToCol(int num) {
-        return num % N;
-    } // End intToCol
-
-    // Updates site 2's connections after checking site 1's connections
-    // Sites 1 and 2 must be connected
+    /**
+     * Updates site 2's connections according to those of site 1
+     * Sites 1 and 2 must be connected
+     * @param site1Row Site 1's row number
+     * @param site1Col Site 1's column number
+     * @param site2Row Site 2's row number
+     * @param site2Col Site 2's column number
+     */
     private void updateConnections(int site1Row, int site1Col,
                                    int site2Row, int site2Col) {
 
-        // Check that site 1 is not the same as site 2
-        if (!(site1Row == site2Row && site1Col == site2Col)) {
+        if (isValid(site1Row) && isValid(site1Col)
+                && isValid(site2Row) && isValid(site2Col)) {
 
-            // Update connection to grid's top row
-            if (siteStatus[site1Row][site1Col] % 4 == 2
-                || siteStatus[site1Row][site1Col] % 4 == 3) {
-                siteStatus[site2Row][site2Col] |= 2;
-            }
+            /** Check that site 1 is not the same as site 2 */
+            if (!(site1Row == site2Row && site1Col == site2Col)) {
 
-            // Update connections to grid's bottom row
-            if (siteStatus[site1Row][site1Col] >= 4) {
-                siteStatus[site2Row][site2Col] |= 4;
+                /** Update connection to grid's top row */
+                if (siteStatus[site1Row][site1Col] % 4 == 2
+                        || siteStatus[site1Row][site1Col] % 4 == 3) {
+                    siteStatus[site2Row][site2Col] |= 2;
+                }
+
+                /** Update connection to grid's bottom row */
+                if (siteStatus[site1Row][site1Col] >= 4) {
+                    siteStatus[site2Row][site2Col] |= 4;
+                }
+
             }
 
         }
 
-    } // End updateConnections
+    } /** End updateConnections */
 
-    // Runs some unit tests
-    public static void main(String[] args) {
-        Percolation system = new Percolation(3);
-        system.open(0, 0);
-        system.open(1, 0);
-        system.open(1, 2);
-        system.open(2, 2);
-        system.open(1, 1);
-        system.isFull(2, 2);
-        system.printGrid();
-    } // End main
+    /************************ End of Helper Methods *************************/
 
-} // End Percolation class
+} /** End Percolation class */
